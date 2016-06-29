d3f2a82cb2e8d5f28750de2cf4168ea1d1d364a3
Katsiaryna Dailid <Katsiaryna_Dailid@epam.com>
2016-06-29 20:21:24 +0300

    first commit

---
+++ HW_29.06_1/HW_29.06_1/Checks.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+using System.IO;
+
+
+namespace HW_29._06_1
+{
+    class Checks
+    {
+
+        public string CheckTheExistingOfDirectory(String path )
+        {
+            bool exist = false;
+            String newPath, dir;
+           
+            do
+            {
+                Console.WriteLine("Enter the name of the directory: ");
+                dir = Console.ReadLine();
+                newPath = path + dir;
+                if (Directory.Exists(newPath))
+                {
+                    Console.WriteLine("The directory is already exist. Enter the 1 if you want to write your file in existing directory."+
+                       "Enter any other key if you want to enter another name for directory.");
+                    String choice = Console.ReadLine();
+
+                    if (choice == "1") { exist = true; }
+                    else { continue; }
+                }
+                else { exist = true; }
+            }
+            while (!exist);
+
+            return newPath;
+        }
+
+
+        public string CheckTheExistingOfFile(String newPath)
+        {
+
+            bool exist = false;
+            String pathToTheFile, fileName; ;
+         
+            do
+            {
+                Console.WriteLine("Enter the name of the file:");
+                fileName = Console.ReadLine();
+                pathToTheFile = newPath +"\\"+ fileName;
+                
+                if (File.Exists(pathToTheFile))
+                {
+                    Console.WriteLine("This file is already exist. Enter the 1 if you want to rewrite your file." +
+                        "Enter any other key if you want to enter another name for file.");
+                    String choice = Console.ReadLine();
+
+                    if (choice == "1") { exist = true; }
+                    else { continue; }
+                }
+                else { exist = true; }
+            }
+            while (!exist);
+            return pathToTheFile;
+        }
+
+
+        public string CheckTheFileWithInformation() {
+            String pathInform;
+            bool exist = false;           
+            do
+            {
+                Console.WriteLine("Enter the path to the file from what you would like to read :");
+                pathInform = Console.ReadLine();
+              
+                if (!File.Exists(pathInform))
+                {
+                    Console.WriteLine("This file is not exist. Try again...");                
+                }
+                else { exist = true; }
+            }
+            while (!exist);
+
+            return pathInform;
+        }
+
+
+        public void WriteInAFile(String pathToRead, String pathToWrite) {
+
+            int counter = 0;
+            string line;
+
+            using (StreamWriter strWriter = new StreamWriter(pathToWrite))
+            {
+                StreamReader strReader = new StreamReader(pathToRead);
+                while ((line = strReader.ReadLine()) != null && counter < 20)
+                {
+
+                    strWriter.WriteLine(line);
+                    Console.WriteLine(line);
+                    counter++;
+
+                }
+
+                strReader.Close();
+            }
+
+        }
+
+    }
+}
---
+++ HW_29.06_1/HW_29.06_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+
+/*  TODO:
+   
+      1.Help
+      2.Testing
+     
+     
+     */
+
+namespace HW_29._06_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String path, newPath, pathToWrite, pathToRead;
+           
+            path = Data.Path; //считали путь из ресурсного файла
+            while (!Directory.Exists(path)) { //проверили путь
+                Console.WriteLine("Your path do not exist. You should enter the existing path in resours file.");
+                Environment.Exit(-1);
+            }
+
+            Checks checkObj = new Checks();           
+            newPath = checkObj.CheckTheExistingOfDirectory(path);
+
+            DirectoryInfo dir = Directory.CreateDirectory(newPath);
+            pathToWrite = checkObj.CheckTheExistingOfFile(newPath);
+
+
+            pathToRead = checkObj.CheckTheFileWithInformation();
+
+            checkObj.WriteInAFile(pathToRead, pathToWrite);
+          
+                Console.ReadLine();
+
+
+
+        }
+
+
+       
+
+    }
+}
